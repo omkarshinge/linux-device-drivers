79517f189824562cf277a8096ee87a043b2adddc
Omkar Shinge <oshinge@gmail.com>
2021-11-26 16:16:57 +0530

    Fix errors in hello_world.c

--- hello_world/hello_world.c
+++ hello_world/hello_world.c
@@ -7,14 +7,14 @@ MODULE_AUTHOR("Omkar Shinge");
 MODULE_DESCRIPTION("Hello World LKM");
 
 // Following function is called when module is loaded into the kernel
-static int __init ModuleInit(){
-    printk("Hello World LKM initialized");
+static int __init ModuleInit(void){
+    printk("Hello World LKM initialized\n");
     return 0;
 }
 
 // Following function is called when module is removed from the kernel
-static void __exit ModuleExit(){
-    printk("Hello World LKM exited");
+static void __exit ModuleExit(void){
+    printk("Hello World LKM exited\n");
 }
 
 module_init(ModuleInit);
